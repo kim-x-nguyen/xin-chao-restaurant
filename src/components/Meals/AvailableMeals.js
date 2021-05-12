e9760b1025d23e151adbdbbf9a7b99b08bcd0ae7
P2356 <p2356@student.jamk.fi>
2021-05-12 21:38:27 +0300

    Delete dummy meals array

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,32 +2,7 @@ import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 15.99,
-    },
-    {
-        id: 'm2',
-        name: 'Pizza Capricciosa',
-        description: 'This pizza is topped with olives, artichoke hearts, half of a boiled egg, prosciutto and mushrooms.',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Hobotnica na novaljski',
-        description: 'Hobotnica na novaljski is a traditional Croatian dish originating from Novalja on the island of Pag',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Polvo à Lagareiro',
-        description: 'This classic Portuguese dish is made in the style of Lagareiro, hence the name.',
-        price: 18.99,
-    },
-];
+
 
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal =>
